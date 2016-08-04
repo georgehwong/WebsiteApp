01f91a188eb57207c91302af45d9da3a0a9e76cb
georgehwong <huangshangru@126.com>
2016-08-04 00:17:46 -0700

    Update: Use Fragment, ViewPagerIndicator as app framework

--- app/src/main/java/webapp/shangru/project/com/websiteapp/MainActivity.java
+++ app/src/main/java/webapp/shangru/project/com/websiteapp/MainActivity.java
@@ -1,13 +1,27 @@
 package webapp.shangru.project.com.websiteapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends AppCompatActivity {
+import com.viewpagerindicator.TabPageIndicator;
+
+public class MainActivity extends FragmentActivity {
+
+    private TabPageIndicator mIndicator;
+    private ViewPager mViewPager;
+    private FragmentPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mIndicator = (TabPageIndicator) findViewById(R.id.id_indicator);
+        mViewPager = (ViewPager) findViewById(R.id.id_pager);
+        mAdapter = new TabAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mAdapter);
+        mIndicator.setViewPager(mViewPager, 0);
     }
 }
---
+++ app/src/main/java/webapp/shangru/project/com/websiteapp/TabAdapter.java
@@ -0,0 +1,32 @@
+package webapp.shangru.project.com.websiteapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Shangru on 8/3/16.
+ */
+public class TabAdapter extends FragmentPagerAdapter {
+    public static final String[] TITLES = new String[]{"Test_0", "Test_1", "Test_2", "Test_3", "Test_4"};
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+        MainFragment fragment = new MainFragment(arg0 + 1);
+        return fragment;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return TITLES[position % TITLES.length];
+    }
+
+    @Override
+    public int getCount() {
+        return TITLES.length;
+    }
+}
